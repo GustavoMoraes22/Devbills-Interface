1d57e121ba11e4f8883015fac8d27026ac086f0c
GustavoMoraes22 <DeveloperGustavoM@hotmail.com>
2024-06-21 20:58:54 -0300

    add button and input

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,15 +1,49 @@
+import { InputMask } from '@react-input/mask';
+
 import { Button } from '../../components/button';
+import { ButtonIcon } from '../../components/button-icon';
+import { Input } from '../../components/input';
 import { Logo } from '../../components/logo';
-import { Header } from './styles';
+import { Title } from '../../components/title';
+import { Header, Main, Section, Filters, InputGroup } from './styles';
 
 export function Home() {
   return (
-    <Header>
-      <Logo />
-      <div>
-        <Button>Nova transação</Button>
-        <Button>Nova categoria</Button>
-      </div>
-    </Header>
+    <>
+      <Header>
+        <Logo />
+        <div>
+          <Button>Nova transação</Button>
+          <Button>Nova categoria</Button>
+        </div>
+      </Header>
+
+      <Main>
+        <Section>
+          <Filters>
+            <Title title="Saldo" subtitle="Receitas e despesas no período" />
+            <InputGroup>
+              <InputMask
+                component={Input}
+                mask="dd/mm/yyyy"
+                replacement={{ d: /\d/, m: /\d/, y: /\d/ }}
+                variant="dark"
+                label="Inicio"
+                placeholder="dd/mm/yyyy"
+              />
+              <InputMask
+                component={Input}
+                mask="dd/mm/yyyy"
+                replacement={{ d: /\d/, m: /\d/, y: /\d/ }}
+                variant="dark"
+                label="Fim"
+                placeholder="dd/mm/yyyy"
+              />
+              <ButtonIcon />
+            </InputGroup>
+          </Filters>
+        </Section>
+      </Main>
+    </>
   );
 }
